2a1286f20fd23f829e87d227c702d39cff86748a
Tegar Witjaksono <tegarwtj2308@gmail.com>
2024-11-20 00:06:31 +0700

    Commit

--- app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/article/ArticleActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/article/ArticleActivity.kt
@@ -2,25 +2,29 @@ package com.dicoding.picodiploma.loginwithanimation.view.article
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.picodiploma.loginwithanimation.R
 import com.dicoding.picodiploma.loginwithanimation.view.addstory.AddStoryActivity
 import com.dicoding.picodiploma.loginwithanimation.view.main.MainActivity
 import com.dicoding.picodiploma.loginwithanimation.view.maps.MapsActivity
 import com.dicoding.picodiploma.loginwithanimation.view.save.SaveActivity
+import com.dicoding.picodiploma.loginwithanimation.view.welcome.WelcomeActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 class ArticleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article)
 
-
         // Inisialisasi Bottom Navigation
         setupBottomNavigation() // Setup bottom navigation
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNavigationView.selectedItemId = R.id.action_article
     }
+
     private fun setupBottomNavigation() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
@@ -30,7 +34,7 @@ class ArticleActivity : AppCompatActivity() {
                     true
                 }
                 R.id.action_article -> {
-                    true
+                    true // Artikel sudah aktif
                 }
                 R.id.action_add_story -> {
                     startActivity(Intent(this, AddStoryActivity::class.java))
@@ -47,5 +51,32 @@ class ArticleActivity : AppCompatActivity() {
                 else -> false
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu) // Memuat menu
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                logout() // Panggil fungsi logout saat item menu diklik
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun logout() {
+        // Hapus sesi pengguna dari SharedPreferences
+        val sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.clear() // Menghapus semua data
+        editor.apply() // Simpan perubahan
+
+        // Arahkan kembali ke WelcomeActivity
+        startActivity(Intent(this, WelcomeActivity::class.java))
+        finish() // Tutup ArticleActivity
     }
 }
